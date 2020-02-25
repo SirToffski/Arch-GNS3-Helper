e059efa4531b68f89743fbe76c95ae0c74af523f
SirToffski <47118864+SirToffski@users.noreply.github.com>
2020-02-25 16:01:27 -0400

    Separate script parts into fuctions

--- install-gns3.sh
+++ install-gns3.sh
@@ -4,279 +4,309 @@
 
 # AUR access and YAY are required.
 
-my_repo_folder=$(find /home -type d -name Arch-GNS3-Helper)
+my_repo_folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 source "$my_repo_folder"/docs/colours.sh
 
 my_separator="+----------------------------------------+"
 
-latest_GNS3_release=v2.1.20
+latest_GNS3_release=v2.2.5
 
-check_for_yay=$(pacman -Qe | grep -c yay)
+yay_status_check() {
+  check_for_yay=$(pacman -Qe | grep -c yay)
 
-if [[ "$check_for_yay" -lt 1 ]]; then
-  echo -e "
+  if [[ $check_for_yay -lt 1 ]]; then
+    printf %b\\n "
   Yay does not appear to be installed.
 
   Would you like the script to install Yay pefore proceeding?
 
   1 = yes, 2 = no"
 
-  read -r install_yay
-  if [[ "$install_yay" == 1 ]]; then
-    echo -e "
+    read -r install_yay
+    if [[ $install_yay == 1 ]]; then
+      printf %b\\n "
     $my_separator
     ${BCyan}Installing YAY${Color_Off}
     $my_separator"
-    sleep 2
-    git clone https://aur.archlinux.org/yay.git
-    cd yay || exit
-    makepkg -si
-  else
-    echo -e "
+      sleep 2
+      git clone https://aur.archlinux.org/yay.git
+      cd yay || exit
+      makepkg -si
+    else
+      printf %b\\n "
     YAY is required for this script. Either install it manually then re-run the script, or let the script install it for you.
 
     Ending the script...."
-    exit
+      exit
+    fi
   fi
-fi
+}
 
-echo -e "
+intro() {
+  printf %b\\n "
 $my_separator
 ${IWhite}The script will perform installation steps as described in https://medium.com/@Ninja/install-gns3-on-arch-manjaro-linux-the-right-way-c5a3c4fa337d
 
 You are encouraged to either read through the script or the article to make sure you understand the steps involved.${Color_Off}
 
-$my_separator
-"
-read -n 1 -s -r -p "
-Press any key to continue or CTRL+C to exit the script"
+$my_separator\n"
 
-echo -e "
+  read -n 1 -s -r -p "
+Press any key to continue or CTRL+C to exit the script"
+}
+
+install_dynamips() {
+  printf %b\\n "
 $my_separator
 ${BCyan}Installing dynamips${Color_Off}
 $my_separator
 "
-sleep 2
+  sleep 2
 
-sudo pacman -S libelf libpcap cmake --noconfirm
-yay -S dynamips --noconfirm
-sudo setcap cap_net_admin,cap_net_raw=ep "$(command -v dynamips)"
+  sudo pacman -S libelf libpcap cmake --noconfirm --needed
+  yay -S dynamips --noconfirm --needed
+  sudo setcap cap_net_admin,cap_net_raw=ep "$(command -v dynamips)"
 
-cd "$HOME" || exit
-check_for_dynamips=$(dynamips 2> /dev/null | grep -c version)
-if [[ "$check_for_dynamips" -lt 1 ]]; then
-  echo -e "${On_Red}
+  cd "$HOME" || exit
+  check_for_dynamips=$(dynamips 2>/dev/null | grep -c version)
+  if [[ $check_for_dynamips -lt 1 ]]; then
+    printf %b\\n "${On_Red}
   Unable to find dynamips after isntall....
   Aborting the script${Color_Off}"
-  exit
-fi
+    exit
+  fi
+}
 
-# Installing VPCS
-echo -e "
+install_vpcs() {
+  # Installing VPCS
+  printf %b\\n "
 $my_separator
 ${BCyan}Installing VPCS${Color_Off}
 $my_separator
 "
-sleep 2
-yay -S vpcs --noconfirm
-cd "$HOME" || exit
-check_for_vpcs=$(type vpcs | grep -c "vpcs is /usr/bin/vpcs")
-if [[ "$check_for_vpcs" -lt 1 ]]; then
-  echo -e "${On_Red}
+  sleep 2
+  yay -S vpcs --noconfirm --needed
+  cd "$HOME" || exit
+  check_for_vpcs=$(type vpcs | grep -c "vpcs is /usr/bin/vpcs")
+  if [[ $check_for_vpcs -lt 1 ]]; then
+    printf %b\\n "${On_Red}
   Unable to find VPCS after isntall....
   Aborting the script${Color_Off}"
-  exit
-fi
+    exit
+  fi
+}
 
-# Install IOUYAP
-echo -e "
+install_iouyap() {
+  # Install IOUYAP
+  printf %b\\n "
 $my_separator
 ${BCyan}Installing IOUYAP${Color_Off}
 $my_separator
 "
-sleep 2
-sudo pacman -S iniparser --noconfirm
-yay -S iouyap --noconfirm
-cd "$HOME" || exit
-sudo setcap cap_net_admin,cap_net_raw=ep "$(command -v iouyap)"
-check_for_iouyap=$(iouyap -V | grep -c iouyap)
-if [[ "$check_for_iouyap" -lt 1 ]]; then
-  echo -e "${On_Red}
+  sleep 2
+  sudo pacman -S iniparser --noconfirm --needed
+  yay -S iouyap --noconfirm --needed
+  cd "$HOME" || exit
+  sudo setcap cap_net_admin,cap_net_raw=ep "$(command -v iouyap)"
+  check_for_iouyap=$(iouyap -V | grep -c iouyap)
+  if [[ $check_for_iouyap -lt 1 ]]; then
+    printf %b\\n "${On_Red}
   Unable to find IOUYAP after install....
   Aborting the script${Color_Off}"
-  exit
-fi
+    exit
+  fi
+}
 
-# Install IOL Dependencies
-echo -e "
+install_iol_dependencies() {
+  # Install IOL Dependencies
+  printf %b\\n "
 $my_separator
 ${BCyan}Installing IOL Dependencies${Color_Off}
 $my_separator
 "
-sleep 2
-sudo pacman -S lib32-openssl lib32-gcc-libs --noconfirm
-sudo ln -s /usr/lib32/libcrypto.so.1.0.0 /usr/lib32/libcrypto.so.4
-sudo sysctl net.unix.max_dgram_qlen=10000
-sudo tee -a /etc/sysctl.d/99-sysctl.conf > /dev/null << EOL
+  sleep 2
+  sudo pacman -S lib32-openssl lib32-gcc-libs --noconfirm --needed
+  sudo ln -s /usr/lib32/libcrypto.so.1.0.0 /usr/lib32/libcrypto.so.4
+  sudo sysctl net.unix.max_dgram_qlen=10000
+  sudo tee -a /etc/sysctl.d/99-sysctl.conf >/dev/null <<EOL
 # Prevent EXCESSCOLL error for IOL
 net.unix.max_dgram_qlen=10000
 EOL
-check_excesscoll_1=$(sysctl net.unix.max_dgram_qlen | grep -c 10000)
-check_excesscoll_2=$(tail -2 /etc/sysctl.d/99-sysctl.conf | grep -c 10000)
+  check_excesscoll_1=$(sysctl net.unix.max_dgram_qlen | grep -c 10000)
+  check_excesscoll_2=$(tail -2 /etc/sysctl.d/99-sysctl.conf | grep -c 10000)
 
-if [[ "$check_excesscoll_1" -lt 1 ]] || [[ "$check_excesscoll_2" -lt 1 ]] ; then
-  echo -e "${On_Red}Excesscoll error prevention did not work....
+  if [[ $check_excesscoll_1 -lt 1 ]] || [[ $check_excesscoll_2 -lt 1 ]]; then
+    printf %b\\n "${On_Red}Excesscoll error prevention did not work....
   Something is not working correct..
 
   Ending the excipt${Color_Off}"
-  exit
-fi
+    exit
+  fi
+}
 
-# Install uBridge
-sleep 2
-echo -e "
+install_ubridge() {
+  # Install uBridge
+  sleep 2
+  printf %b\\n "
 $my_separator
 ${BCyan}Installing uBridge${Color_Off}
 $my_separator
 "
-yay -S ubridge --noconfirm
-cd "$HOME" || exit
-check_for_ubridge=$(ubridge -v | grep -c ubridge)
-if [[ "$check_for_ubridge" -lt 1 ]]; then
-  echo -e "${On_Red}
+  yay -S ubridge --noconfirm --needed
+  cd "$HOME" || exit
+  check_for_ubridge=$(ubridge -v | grep -c ubridge)
+  if [[ $check_for_ubridge -lt 1 ]]; then
+    printf %b\\n "${On_Red}
   uBridge was not found after installation.
   Something did not work correctly.
 
   Edning the script....${Color_Off}"
-  exit
-fi
+    exit
+  fi
+}
 
-# Install QEMU
-echo -e "
+install_qemu() {
+  # Install QEMU
+  printf %b\\n "
 $my_separator
 ${BCyan}Installing QEMU${Color_Off}
 $my_separator
 "
-sleep 2
-sudo pacman -S qemu --noconfirm
+  sleep 2
+  sudo pacman -S qemu --noconfirm --needed
+}
 
-
-# Install docker
-echo -e "
+install_docker() {
+  # Install docker
+  printf %b\\n "
 $my_separator
 ${BCyan}Installing Docker${Color_Off}
 $my_separator
 "
-sleep 2
-sudo pacman -S docker --noconfirm
-sudo systemctl enable docker.service
-sudo systemctl start docker.service
-sudo gpasswd -a "$USER" docker
+  sleep 2
+  sudo pacman -S docker --noconfirm --needed
+  sudo systemctl enable docker.service
+  sudo systemctl start docker.service
+  sudo gpasswd -a "$USER" docker
+}
 
-# Install Wireshark
-echo -e "
+install_wireshark() {
+  # Install Wireshark
+  printf %b\\n "
 $my_separator
 ${BCyan}Installing Wireshark${Color_Off}
 $my_separator
 "
-sleep 2
-sudo pacman -S wireshark-qt --noconfirm
-sudo gpasswd -a "$USER" wireshark
+  sleep 2
+  sudo pacman -S wireshark-qt --noconfirm --needed
+  sudo gpasswd -a "$USER" wireshark
+}
 
-# Install python-pypi2pkgbuild
-echo -e "
+install_python-pypi2pkgbuild() {
+  # Install python-pypi2pkgbuild
+  printf %b\\n "
 $my_separator
 ${BCyan}Installing python-pypi2pkgbuild${Color_Off}
 $my_separator
 "
-sleep 2
-yay -S python-pypi2pkgbuild --noconfirm
-sudo pacman -S python-wheel --noconfirm
-yay -S python-zipstream --noconfirm
-# Install GNS3 Dependencies
-echo -e "
+  sleep 2
+  yay -S python-pypi2pkgbuild --noconfirm --needed
+  sudo pacman -S python-wheel --noconfirm --needed
+  yay -S python-zipstream --noconfirm --needed
+}
+
+install_gns_dependencies() {
+  # Install GNS3 Dependencies
+  printf %b\\n "
 $my_separator
 ${BCyan}Installing GNS3 Dependencies${Color_Off}
 $my_separator
 "
-sleep 2
-sudo pacman -S qt5-svg qt5-websockets python-pip python-pyqt5 python-sip --noconfirm
-sudo pacman -S git --noconfirm
+  sleep 2
+  sudo pacman -S qt5-svg qt5-websockets python-pip python-pyqt5 python-sip --noconfirm --needed
+  sudo pacman -S git --noconfirm --needed
+}
 
-# Prepare to install GNS3-server
-echo -e "
+install_gns3_server() {
+  # Prepare to install GNS3-server
+  printf %b\\n "
 $my_separator
 ${BCyan}Prepare to install GNS3-server${Color_Off}
 $my_separator
 "
-sleep 2
-mkdir -p "$HOME"/GNS3-Dev && cd "$_" || exit
-git clone https://github.com/GNS3/gns3-server.git
-cd gns3-server || exit
-git checkout "$latest_GNS3_release"
-sudo pkgfile --update
-echo -e "
+  sleep 2
+  mkdir -p "$HOME"/GNS3-Dev && cd "$_" || exit
+  git clone https://github.com/GNS3/gns3-server.git
+  cd gns3-server || exit
+  git checkout "$latest_GNS3_release"
+  sudo pkgfile --update
+  printf %b\\n "
 $my_separator
 ${BCyan}Install GNS3-server${Color_Off}
 $my_separator
 "
-sleep 2
-PKGEXT=.pkg.tar pypi2pkgbuild.py -g cython -b /tmp/pypi2pkgbuild/ -f git+file://"$HOME"/GNS3-Dev/gns3-server
-sleep 5
-# Install GNS3 GUI
-echo -e "
+  sleep 2
+  PKGEXT=.pkg.tar pypi2pkgbuild.py -g cython -b /tmp/pypi2pkgbuild/ -f git+file://"$HOME"/GNS3-Dev/gns3-server
+  sleep 2
+}
+
+install_gns3_gui() {
+  # Install GNS3 GUI
+  printf %b\\n "
 $my_separator
 ${BCyan}Install GNS3-GUI${Color_Off}
 $my_separator
 "
-sleep 2
-git clone https://github.com/GNS3/gns3-gui.git
-cd gns3-gui || exit
-git checkout "$latest_GNS3_release"
-sudo pkgfile --update
-PKGEXT=.pkg.tar pypi2pkgbuild.py -g cython -b /tmp/pypi2pkgbuild/ -f git+file://"$HOME"/GNS3-Dev/gns3-server/gns3-gui
+  sleep 2
+  git clone https://github.com/GNS3/gns3-gui.git
+  cd gns3-gui || exit
+  git checkout "$latest_GNS3_release"
+  sudo pkgfile --update
+  PKGEXT=.pkg.tar pypi2pkgbuild.py -g cython -b /tmp/pypi2pkgbuild/ -f git+file://"$HOME"/GNS3-Dev/gns3-server/gns3-gui
+}
 
-# Verifying GNS3 installation
-echo -e "
+verify_gns3_installation() {
+  # Verifying GNS3 installation
+  printf %b\\n "
 $my_separator
 ${BCyan}Verifying the installation.${Color_Off}
 $my_separator
 "
-sleep 3
-check_for_gns3=$(pacman -Qe | grep -c python-gns3)
-if [[ "$check_for_gns3" -lt 2 ]]; then
-  echo -e "${On_Red}
+  sleep 3
+  check_for_gns3=$(pacman -Qe | grep -c python-gns3)
+  if [[ $check_for_gns3 -lt 2 ]]; then
+    printf %b\\n "${On_Red}
   It appears the installation was either completed partially or has not been completed at all....
 
   Checking further${Color_Off}"
-  sleep 1
-  check_for_gns3_gui=$(pacman -Qe | grep -c python-gns3-gui)
-  if [[ "$check_for_gns3_gui" -lt 1 ]]; then
-    echo -e "${On_Red}
+    sleep 1
+    check_for_gns3_gui=$(pacman -Qe | grep -c python-gns3-gui)
+    if [[ $check_for_gns3_gui -lt 1 ]]; then
+      printf %b\\n "${On_Red}
     GNS 3 GUI was not installed...
 
     attempting to re-install${Color_Off}"
-    PKGEXT=.pkg.tar pypi2pkgbuild.py -g cython -b /tmp/pypi2pkgbuild/ -f git+file://"$HOME"/GNS3-Dev/gns3-server/gns3-gui
-    sleep 1
-  fi
-  check_for_gns3_server=$(pacman -Qe | grep -c python-gns3-server)
-  if [[ "$check_for_gns3_server" -lt 1 ]]; then
-    echo -e "${On_Red}
+      PKGEXT=.pkg.tar pypi2pkgbuild.py -g cython -b /tmp/pypi2pkgbuild/ -f git+file://"$HOME"/GNS3-Dev/gns3-server/gns3-gui
+      sleep 1
+    fi
+    check_for_gns3_server=$(pacman -Qe | grep -c python-gns3-server)
+    if [[ $check_for_gns3_server -lt 1 ]]; then
+      printf %b\\n "${On_Red}
     GNS 3 Server was not installed...
 
       attempting to re-install${Color_Off}"
       PKGEXT=.pkg.tar pypi2pkgbuild.py -g cython -b /tmp/pypi2pkgbuild/ -f git+file://"$HOME"/GNS3-Dev/gns3-server
       sleep 1
-  fi
-else
-  echo -e "${IGreen}
+    fi
+  else
+    printf %b\\n "${IGreen}
   Everything looks alright. If you are using Gnome or Budgie, the script can make a launcher icon for you.
 
   1 = yes, 2 = no${Color_Off}"
-  read -r make_launcher
-  if [[ "$make_launcher" == 1 ]]; then
-    sudo tee -a /usr/share/applications/gns3.desktop > /dev/null << EOL
+    read -r make_launcher
+    if [[ $make_launcher == 1 ]]; then
+      sudo tee -a /usr/share/applications/gns3.desktop >/dev/null <<EOL
 [Desktop Entry]
 Type=Application
 Encoding=UTF-8
@@ -288,9 +318,30 @@ Icon=gns3
 Terminal=false
 Categories=Application;Network;Qt;
 EOL
-  fi
-  echo -e "${IGreen}
+    fi
+    printf %b\\n "${IGreen}
   Installation has been completed!
 
   Please reboot your PC...${Color_Off}"
-fi
+  fi
+}
+
+main() {
+  yay_status_check
+  intro
+  install_dynamips
+  install_vpcs
+  install_iouyap
+  install_iol_dependencies
+  install_ubridge
+  install_qemu
+  install_docker
+  install_wireshark
+  install_python-pypi2pkgbuild
+  install_gns_dependencies
+  install_gns3_server
+  install_gns3_gui
+  verify_gns3_installation
+}
+
+main
